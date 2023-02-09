d231bb321b43716ccfd7b67a3605e344462efa2e
André Caldas <andre.em.caldas@gmail.com>
2023-02-09 11:21:39 -0300

    New workbench.

--- freecad/ArchWorkflow/init_gui.py
+++ freecad/ArchWorkflow/init_gui.py
@@ -1,30 +1,29 @@
 import os
 import FreeCADGui as Gui
 import FreeCAD as App
-from freecad.workbench_starterkit import ICONPATH
+from freecad.arch_workflow import ICONPATH
 
 
-class TemplateWorkbench(Gui.Workbench):
+class ArchWorkflow(Gui.Workbench):
     """
     class which gets initiated at startup of the gui
     """
 
-    MenuText = "template workbench"
-    ToolTip = "a simple template workbench"
+    MenuText = "Arch Workflow"
+    ToolTip = "a workflow and a collection of workbenches for projecting a house"
     Icon = os.path.join(ICONPATH, "template_resource.svg")
     toolbox = []
 
     def GetClassName(self):
-        return "Gui::PythonWorkbench"
+        return "Gui::ArchWorkflow"
 
     def Initialize(self):
         """
         This function is called at the first activation of the workbench.
         here is the place to import all the commands
         """
-        from freecad.workbench_starterkit import my_numpy_function
-        App.Console.PrintMessage("switching to workbench_starterkit\n")
-        App.Console.PrintMessage("run a numpy function: sqrt(100) = {}\n".format(my_numpy_function.my_foo(100)))
+#        from freecad.workbench_starterkit import my_numpy_function
+        App.Console.PrintMessage("Switching to ArchWorkflow.\n")
 
         self.appendToolbar("Tools", self.toolbox)
         self.appendMenu("Tools", self.toolbox)
@@ -42,4 +41,4 @@ class TemplateWorkbench(Gui.Workbench):
         pass
 
 
-Gui.addWorkbench(TemplateWorkbench())
+Gui.addWorkbench(ArchWorkflow())
--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import os
 # Packages using the same name here cannot be installed together
 
 version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
-                            "freecad", "workbench_starterkit", "version.py")
+                            "freecad", "ArchWorkflow", "version.py")
 with open(version_path) as fp:
     exec(fp.read())
 
-setup(name='freecad.workbench_starterkit',
+setup(name='freecad.ArchWorkflow',
       version=str(__version__),
       packages=['freecad',
-                'freecad.workbench_starterkit'],
-      maintainer="looooo",
-      maintainer_email="sppedflyer@gmail.com",
-      url="https://github.com/FreeCAD/Workbench-Starterkit",
-      description="template for a freecad extensions, installable with pip",
+                'freecad.ArchWorkflow'],
+      maintainer="André Caldas",
+      maintainer_email="andre.em.caldas@gmail.com",
+      url="https://github.com/andre-caldas/freecad_arch_workflow",
+      description="a flow of workbenches to project a house",
       install_requires=['numpy'], # should be satisfied by FreeCAD's system dependencies already
       include_package_data=True)
